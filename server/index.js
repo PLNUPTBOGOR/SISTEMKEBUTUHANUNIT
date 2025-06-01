c02a845217a4cc349e6c4ef6a8b7911c250da0aa
PLNUPTBOGOR <kebutuhanunit@gmail.com>
2025-06-01 22:22:47 +0700

    updatee index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
@@ -17,53 +16,44 @@ import addressRouter from './route/address.route.js'
 import orderRouter from './route/order.route.js'
 import analyticsRouter from './route/analytics.route.js'
 
+
+
 const app = express()
-
-const allowedOrigins = [
-  process.env.FRONTEND_URL, // dev frontend
-  'https://sistem-kebutuhan-pln-sg1o.vercel.app' // deployed frontend
-]
-
 app.use(cors({
-  credentials: true,
-  origin: function(origin, callback) {
-    // allow requests with no origin (like curl, postman)
-    if (!origin) return callback(null, true)
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`
-      return callback(new Error(msg), false)
-    }
-    return callback(null, true)
-  }
+    credentials : true,
+    origin : process.env.FRONTEND_URL
 }))
-
 app.use(express.json())
 app.use(cookieParser())
-app.use(morgan()) // untuk development
+// app.use(morgan())
+app.use(morgan()) // cocok untuk development
 app.use(helmet({
-  crossOriginResourcePolicy: false
+    crossOriginResourcePolicy : false
 }))
 
-const PORT = process.env.PORT || 8080
+const PORT = 8080 || process.env.PORT 
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Server is running on port " + PORT
-  })
+app.get("/",(request,response)=>{
+    ///server to client
+    response.json({
+        message : "Server is running " + PORT
+    })
 })
 
-app.use("/api/user", userRouter)
-app.use("/api/category", categoryRouter)
-app.use("/api/file", uploadRouter)
-app.use("/api/subcategory", subCategoryRouter)
-app.use("/api/product", productRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/address", addressRouter)
-app.use("/api/order", orderRouter)
+app.use("/api/user",userRouter)
+app.use("/api/category",categoryRouter)
+app.use("/api/file",uploadRouter)
+app.use("/api/subcategory",subCategoryRouter)
+app.use("/api/product",productRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/address",addressRouter)
+app.use("/api/order",orderRouter)
 app.use("/api/analytics", analyticsRouter)
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server is running on port", PORT)
-  })
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is running",PORT)
+    })
 })
+
